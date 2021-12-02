47f850283520d81c32d201e1d2998dbffd17e208
zVuDa <mgoncao.sansus3@gmail.com>
2021-12-02 11:34:33 +0100

    slides

--- js/main.js
+++ js/main.js
@@ -1,33 +1,7 @@
  "use strict"
  const hide = document.querySelector(".hide");
 
- var slideIndex = 1;
- showSlides(slideIndex);
-
- // Next/previous controls
- function plusSlides(n) {
-     showSlides(slideIndex += n);
- }
-
- // Thumbnail image controls
- function currentSlide(n) {
-     showSlides(slideIndex = n);
- }
-
- function showSlides(n) {
-     //   var i;
-     //   var slides = document.getElementsByClassName("myslides");
-     //   if (n > slides.length) {slideIndex = 1} 
-     //   if (n < 1) {slideIndex = slides.length}
-     //   for (i = 0; i < slides.length; i++) {
-     //       slides[i].style.display = "none"; 
-     //   }
-     //   slides[slideIndex-1].style.display = "block"; 
-     //   slides[slideIndex-2].style.display = "block"; 
-
- }
-
-
+//BTN NEXT 50%
  document.querySelector("#next1").onclick = e => {
      e.preventDefault();
      let indice = 0;
@@ -46,11 +20,68 @@
          hijos[i].classList.remove('hide');
 
      }
-     // console.log(indice)
-     // if(indice===8){
-     //     indice = 0;
-     // }
-
-     //hide.style = `display:block`
-
  }
+ //BTN NEXT 40%
+ document.querySelector("#next2").onclick = e => {
+    e.preventDefault();
+    let indice = 0;
+    let hijos = e.target.parentElement.parentElement.children;
+    for (let i = 0, tam = hijos.length; i < tam - 2; i++) {
+        if (hijos[i].classList.contains('hide') !== true) {
+            hijos[i].classList.add('hide');
+            indice = i;
+            if (indice === 8) {
+                indice = 0;
+            }
+
+        }
+    }
+    for (let i = indice; i < indice + 3; i++) {
+        hijos[i].classList.remove('hide');
+
+    }
+}
+ //BTN NEXT 30%
+ document.querySelector("#next3").onclick = e => {
+    e.preventDefault();
+    let indice = 0;
+    let hijos = e.target.parentElement.parentElement.children;
+    for (let i = 0, tam = hijos.length; i < tam - 2; i++) {
+        if (hijos[i].classList.contains('hide') !== true) {
+            hijos[i].classList.add('hide');
+            indice = i;
+            if (indice === 8) {
+                indice = 0;
+            }
+
+        }
+    }
+    for (let i = indice; i < indice + 3; i++) {
+        hijos[i].classList.remove('hide');
+
+    }
+}
+ //BTN PREV 50%
+ document.querySelector("#prev1").onclick = e => {
+    e.preventDefault();
+    let indice = 8;
+    let hijos = e.target.parentElement.parentElement.children;
+    console.log(hijos[8])
+    for (let i = 8, tam = hijos.length; i < tam; i--) {
+        if (hijos[i].classList.contains('dfgd') == true) {
+            hijos[i].classList.remove('hide');
+            indice = i;
+            console.log([i])
+        if(hijos[i].classList.contains(`ljkñ`) == true){hijos[i].classList.add('hide')}
+            if (indice === 0) {
+                indice = 8;
+            }
+
+        }
+    }
+    for (let i = indice; i > indice + 3; i--) {
+       if (hijos[i].classList.contains('hide') !== true){hijos[i].classList.add('hide');};
+
+    }
+}
+    
