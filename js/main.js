4e20e39f3f45b28a2e866a80e88aab7f0d5c15df
zVuDa <mgoncao.sansus3@gmail.com>
2021-12-02 14:07:04 +0100

    responsive slide

--- js/main.js
+++ js/main.js
@@ -66,22 +66,77 @@
     e.preventDefault();
     let indice = 8;
     let hijos = e.target.parentElement.parentElement.children;
-    console.log(hijos[8])
+    console.log(hijos[1])
+    console.log(hijos[1].classList.contains(`ljkñ`))
+
     for (let i = 8, tam = hijos.length; i < tam; i--) {
-        if (hijos[i].classList.contains('dfgd') == true) {
-            hijos[i].classList.remove('hide');
-            indice = i;
+        if (hijos[i].classList.contains(`dfgd`) == true) {
+            hijos[i].classList.remove(`hide`)
+            hijos[i].classList.add(`paca`);
+        console.log(hijos[i].classList)
+        // if(hijos[i].classList.contains(`ljkñ`) == true){hijos[i].classList.add('hide')}
+        //     if (indice === 0) {
+        //         indice = 8;
+        //     }
+
+        }if(hijos[i].classList.contains(`ljkñ`)){hijos[i].classList.add(`hide`)};
+            
+    for (let i = indice; i > indice; i--){
+       if (hijos[i].classList.contains('hide') !== true){hijos[i].classList.remove('hide');};
             console.log([i])
-        if(hijos[i].classList.contains(`ljkñ`) == true){hijos[i].classList.add('hide')}
-            if (indice === 0) {
-                indice = 8;
-            }
+        
+    }}
+}
+//BTN PREV 40%
+document.querySelector("#prev2").onclick = e => {
+    e.preventDefault();
+    let indice = 8;
+    let hijos = e.target.parentElement.parentElement.children;
+    console.log(hijos[1])
+    console.log(hijos[1].classList.contains(`ljkñ`))
 
-        }
-    }
-    for (let i = indice; i > indice + 3; i--) {
-       if (hijos[i].classList.contains('hide') !== true){hijos[i].classList.add('hide');};
+    for (let i = 8, tam = hijos.length; i < tam; i--) {
+        if (hijos[i].classList.contains(`dfgd`) == true) {
+            hijos[i].classList.remove(`hide`)
+            hijos[i].classList.add(`paca`);
+        console.log(hijos[i].classList)
+        // if(hijos[i].classList.contains(`ljkñ`) == true){hijos[i].classList.add('hide')}
+        //     if (indice === 0) {
+        //         indice = 8;
+        //     }
 
-    }
+        }if(hijos[i].classList.contains(`ljkñ`)){hijos[i].classList.add(`hide`)};
+            
+    for (let i = indice; i > indice; i--){
+       if (hijos[i].classList.contains('hide') !== true){hijos[i].classList.remove('hide');};
+            console.log([i])
+        
+    }}
+}
+//BTN PREV 30%
+document.querySelector("#prev3").onclick = e => {
+    e.preventDefault();
+    let indice = 8;
+    let hijos = e.target.parentElement.parentElement.children;
+    console.log(hijos[1])
+    console.log(hijos[1].classList.contains(`ljkñ`))
+
+    for (let i = 8, tam = hijos.length; i < tam; i--) {
+        if (hijos[i].classList.contains(`dfgd`) == true) {
+            hijos[i].classList.remove(`hide`)
+            hijos[i].classList.add(`paca`);
+        console.log(hijos[i].classList)
+        // if(hijos[i].classList.contains(`ljkñ`) == true){hijos[i].classList.add('hide')}
+        //     if (indice === 0) {
+        //         indice = 8;
+        //     }
+
+        }if(hijos[i].classList.contains(`ljkñ`)){hijos[i].classList.add(`hide`)};
+            
+    for (let i = indice; i > indice; i--){
+       if (hijos[i].classList.contains('hide') !== true){hijos[i].classList.remove('hide');};
+            console.log([i])
+        
+    }}
 }
     
